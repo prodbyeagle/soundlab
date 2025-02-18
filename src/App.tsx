f87ba0f7e3c9cbed7c38bf4d93c4dae7713600bc
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-02-18 03:38:38 +0100

    added context "remover", and added the route for the fav page which is coming soon

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,28 @@ import { SoundProvider } from './context/SoundContext';
 import Library from './pages/Library';
 import Settings from './pages/Settings';
 import Titlebar from './components/Titlebar';
+import { useEffect } from 'react';
 
 const App = () => {
+	useEffect(() => {
+		const disableContextMenu = (event: MouseEvent) => {
+			event.preventDefault();
+		};
+
+		document.addEventListener('contextmenu', disableContextMenu);
+		return () => {
+			document.removeEventListener('contextmenu', disableContextMenu);
+		};
+	}, []);
+
 	return (
-		<div className='font-main bg-neutral-950 text-neutral-100 overflow-scroll'>
+		<div className='font-main overflow-scroll bg-neutral-950 text-neutral-100'>
 			<Titlebar />
 			<SoundProvider>
 				<BrowserRouter>
 					<Routes>
 						<Route path='/' element={<Library />} />
+						{/* <Route path='/favorites' element={<Favorites />} /> */}
 						<Route path='/settings' element={<Settings />} />
 					</Routes>
 				</BrowserRouter>
