7712117c609b8ef90a17fd90668957fb7cdc7cdb
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-02-18 04:27:27 +0100

    removed docs cause. idk, and updated some query logic for favs and tags

--- src-tauri/src/db/connection.rs
+++ src-tauri/src/db/connection.rs
@@ -4,22 +4,12 @@ use std::fs;
 use std::path::Path;
 use std::sync::Arc;
 
-/// Structure to manage a connection pool to a SQLite database.
 #[derive(Clone)]
 pub struct DatabasePool {
     db: Arc<Pool<Sqlite>>,
 }
 
 impl DatabasePool {
-    /// Creates a new instance of `DatabasePool` and connects to the specified SQLite database.
-    ///
-    /// # Parameters
-    ///
-    /// - `db_path`: The file path for the SQLite database (e.g., "database.db").
-    ///
-    /// # Returns
-    ///
-    /// Returns a `Result` that either contains a successfully created `DatabasePool` or an error.
     pub async fn new(db_path: &str) -> Result<Self, Error> {
         log(
             LogLevel::Info,
@@ -27,7 +17,6 @@ impl DatabasePool {
             &format!("Attempting to connect to SQLite database at '{}'.", db_path),
         );
 
-        // Prüfe, ob die Datei existiert. Falls nicht, wird sie erstellt.
         if !Path::new(db_path).exists() {
             fs::File::create(db_path).map_err(|e| {
                 log(
@@ -44,7 +33,6 @@ impl DatabasePool {
             );
         }
 
-        // Baue den Verbindungsstring im Format "sqlite://{db_path}"
         let connection_string = format!("sqlite://{}", db_path);
 
         match Pool::<Sqlite>::connect(&connection_string).await {
@@ -70,11 +58,6 @@ impl DatabasePool {
         }
     }
 
-    /// Returns a reference to the SQLite database pool.
-    ///
-    /// # Returns
-    ///
-    /// Returns an `Arc<Pool<Sqlite>>` representing the SQLite database pool.
     pub fn get_db(&self) -> Arc<Pool<Sqlite>> {
         log(
             LogLevel::Debug,
